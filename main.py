79b7890e1b716e90a030ea9a157f2c9d99339e24
Alexey <290199@mail.ru>
2023-06-19 18:17:18 +0500

    Реализация повторной отправки с помощью Celery и Redis.

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
-from sender import Sender
+from sender import Sender_multiplie, Sender_single
 
 app = FastAPI(title="SMS Gateway")
 
+@app.post("/sending")
+async def post_message(address, phone_number, message):
+    return Sender_single().send(address, phone_number, message)
+
 @app.post("/mailing")
 async def post_mailing(address, phone_numbers: list, message):
-    return Sender().send(address, phone_numbers, message)
+    return Sender_multiplie().send(address, phone_numbers, message)
+
--- sender.py
+++ sender.py
@@ -1,14 +1,33 @@
+import requests
 from receiver import To_Google, To_Yandex
+from celery import Celery
 
-class Sender():
+celery_app = Celery('tasks', broker='redis://localhost:6379/0')
+
+repeat_time = 60 # время, которое происходит повторная отправка
+
+class Sender_single():
     @staticmethod
-    def send(address, phone_numbers: list, message):
+    def send(address, phone_number, message):
+        if address == "Google":
+            recepient = To_Google()
+        elif address == "Yandex":
+            recepient = To_Yandex()
+        return [recepient.response(phone_number, message).status_code,
+                recepient.response(phone_number, message).url]
+
+class Sender_multiplie():
+    @celery_app.task(bind=True, default_retry_delay=repeat_time)
+    def send(self, address, phone_numbers: list, message):
         result_list = []
         for number in phone_numbers:
             if address == "Google":
                 recepient = To_Google()
             elif address == "Yandex":
                 recepient = To_Yandex()
-            result = [recepient.response(number, message).status_code, number]
+            try:
+                result = [recepient.response(number, message).status_code, number]
+            except requests.exceptions.ConnectionError as ex:
+                self.retry(exc=ex)
             result_list.append(result)
         return result_list
