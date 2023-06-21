22b3a32ee58623d8eadc73ad7418624f3f57dc4c
Alexey <290199@mail.ru>
2023-06-21 16:00:46 +0500

    Реализация отложенной задачи.

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ app = FastAPI(title="SMS Gateway")
 
 @app.post("/sending")
 async def post_message(address, phone_number, message):
-    return Sender_single().send(address, phone_number, message)
+    return [Sender_single().send_single(address, phone_number, message),
+            Sender_single().send_single.apply_async((address, phone_number, message), countdown=10)]
 
 @app.post("/mailing")
 async def post_mailing(address, phone_numbers: list, message):
-    return Sender_multiplie().send.apply_async((address, phone_numbers, message), countdown=30)
+    return Sender_multiplie().send_multiple.apply_async((address, phone_numbers, message), countdown=10)
--- sender.py
+++ sender.py
@@ -2,13 +2,23 @@ import requests
 from receiver import To_Google, To_Yandex
 from celery import Celery
 
-celery_app = Celery('tasks', broker='redis://localhost:6379/0')
+celery = Celery(
+    '__name__',
+    broker='redis://localhost:6379/0',
+    backend='redis://localhost:6379/0'
+)
+
+celery.conf.task_routes = {
+    'tasks.send_single': {'queue': 'sending'},
+    'tasks.send_multiple': {'queue': 'mailing'},
+}
 
 repeat_time = 60 #время, за которое происходит повторная отправка
 
 class Sender_single():
     @staticmethod
-    def send(address, phone_number, message):
+    @celery.task
+    def send_single(address, phone_number, message):
         if address == "Google":
             recepient = To_Google()
         elif address == "Yandex":
@@ -17,8 +27,8 @@ class Sender_single():
                 recepient.response(phone_number, message).url]
 
 class Sender_multiplie():
-    @celery_app.task(bind=True, default_retry_delay=repeat_time)
-    def send(self, address, phone_numbers: list, message):
+    @celery.task(bind=True, default_retry_delay=repeat_time)
+    def send_multiple(self, address, phone_numbers: list, message):
         result_list = []
         for number in phone_numbers:
             if address == "Google":
